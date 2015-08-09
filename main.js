8f3cb845d7e0a7ca6c41f2526abed699def01823
ALEJANDRO GUTIERREZ <ALEX.GTZ17@GMAIL.COM>
2015-08-09 18:05:17 -0500

    winner function achieved

--- main.js
+++ main.js
@@ -7,27 +7,28 @@ var counter = 0;
 var player1 = [];
 var player2 = [];
 
-
-
 function handleSquareClick(e) {
     if(this.innerHTML === "") {
         if (counter % 2 === 0 ) {
             this.innerHTML = "X";
             player1.push(this.id);
+            checkWinner(player1);
         } else {
             this.innerHTML = "O";
             player2.push(this.id);
+            checkWinner(player2);
         }
         counter++;
-        console.log(player1, player2);
-    } 
-    for(var i = 0; i < winningCombo.length; i++) {
-    	for(var j= 0; j < winningCombo[i].length; j++){
-    	};
-	}
+    }
 }
 
-
+function checkWinner(playerArray) {
+    for(var i = 0; i < winningCombo.length; i++) {
+        if( (playerArray.indexOf(winningCombo[i][0]) > -1)&&(playerArray.indexOf(winningCombo[i][1]) > -1 ) && (playerArray.indexOf(winningCombo[i][2]) > -1)) {
+                alert("We have a winner!");
+        }
+    }
+}
 
 for(var i = 0; i < squareEl.length; i++) {
     squareEl.item(i).addEventListener("click", handleSquareClick);
