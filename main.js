77a8694715538976bfaa1688fdfaa4d054798b4b
ALEJANDRO GUTIERREZ <ALEX.GTZ17@GMAIL.COM>
2015-08-09 21:59:43 -0500

    game logic finished

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var winningCombo = [["0","1","2"], ["3","4","5"], ["6","7","8"],
 var counter = 0;
 var player1 = [];
 var player2 = [];
-
+var game = true;
 
 function handleSquareClick(e) {
     if(this.innerHTML === "" && (game)) {
@@ -21,23 +21,34 @@ function handleSquareClick(e) {
         }
         counter++;
     }
+    if (counter === 9 && game) {
+        alert("DRAW!");
+        game = false;
+    }
 }
 
 function checkWinner(playerArray) {
     for(var i = 0; i < winningCombo.length; i++) {
         if((playerArray.indexOf(winningCombo[i][0]) > -1)&&(playerArray.indexOf(winningCombo[i][1]) > -1 )&&(playerArray.indexOf(winningCombo[i][2]) > -1)) {
-            alert("We have a winner!");
+            if ( counter % 2 === 0 ) {
+                alert("Player 2 is a loser!");
+                game = false;
+            } else {
+                alert("Player 1 is a loser!");
+                game = false;
+            }
         }
     }
 }
 
 function resetGame(e){
-	for(var i = 0; i < squareEl.length; i++){
+	for(var i = 0; i < squareEl.length; i++) {
 		squareEl.item(i).innerHTML = "";
 	}
 	counter = 0;
 	player1 = [];
 	player2 = [];
+            game = true;
 }
 
 for(var i = 0; i < squareEl.length; i++) {
